218beb9f5e5802259b403c77d4fc1382ad169a58
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-08-05 23:25:47 +0530

    classification

---
+++ src/Hotel_Booking_Cancellation_Prediction/constant/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parents[3]
+
+CONFIG_FILE_PATH = ROOT_DIR / "config" / "config.yaml"
+PARAMS_FILE_PATH = ROOT_DIR / "params.yaml"
+SCHEMA_FILE_PATH = ROOT_DIR / "schema.yaml"
+
+# Model Trainer Constants
+MODEL_TRAINER_DIR_NAME = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME = "model.pkl"
